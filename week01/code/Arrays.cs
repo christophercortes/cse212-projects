520534e3ba755a91e2233ef39bb5980ff459c2ca
Christopher Cortes <christopherctes@hotmail.com>
2024-09-23 21:22:40 -0300

    week 01 activites

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,6 +6,7 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
@@ -13,7 +14,17 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // To start you need to create an empty array to store the new multiples
+        // Then you need to create a for loop to iterate the multiples
+        // Later you need to apply the math calculation
+        // Finally return the array
+        double[] NewNumbers = new double[length];
+
+        for (var i = 1; i <= length; i++) {
+            NewNumbers[ i - 1 ] = number * i;
+        }
+
+        return NewNumbers; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +36,19 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Handle the rotation in the loop and rotate the list.
+        // Remove the last element
+        // Remove the element at the beginning.
+        for (int i = 0; i < amount; i++) {
+            int lastElement = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            data.Insert(0, lastElement);
+        }
     }
 }
